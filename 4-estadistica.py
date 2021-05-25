1502683cea4352774b787c3929cc8ef5e024c6d4
Miguel Luque <migue.luque.2014@gmail.com>
2021-05-25 14:29:40 -0500

    Clase 7 - Varianza y Desviación Estándar

--- 4-estadistica.py
+++ 4-estadistica.py
@@ -1,10 +1,26 @@
 import random
+import math
 
 def media(x_arr):
     return sum(x_arr) / len(x_arr)
 
-if __name__ == '__main__':
-    x_arr = [random.randint(1, 20) for _ in range (20)]
+def varianza(x_arr):
     mu = media(x_arr)
+    acum = 0
+    for xi in x_arr:
+        acum += math.pow(xi - mu, 2)
+    return acum / len(x_arr)
+
+def desviacion_estandar(x_arr):
+    return math.sqrt(varianza(x_arr))
+
+if __name__ == '__main__':
+    x_arr = [random.randint(9, 12) for _ in range (20)]
+    mu = media(x_arr)
+    Var = varianza(x_arr)
+    sigma = desviacion_estandar(x_arr)
+
     print(x_arr)
-    print(mu)
+    print(f'Media: {mu}')
+    print(f'Varianza: {Var}')
+    print(f'Desviación estándar: {sigma}')
